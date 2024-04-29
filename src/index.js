1c55275ae87a6cb8648b6b379f42d94bfd1633c4
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-29 12:09:05 +0530

    connected the backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("This is index.js");
+
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLa_nr_0c0kudQSzcGV5hkwq3WH2bRGgo",
+    authDomain: "freidea-pos.firebaseapp.com",
+    projectId: "freidea-pos",
+    storageBucket: "freidea-pos.appspot.com",
+    messagingSenderId: "317401437770",
+    appId: "1:317401437770:web:2657a877ea9fe88cf878b3",
+    measurementId: "G-M8RF0RL2FP"
+  }
+
+initializeApp(firebaseConfig);
