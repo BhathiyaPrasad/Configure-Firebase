bd103e7bd1696f99f8bdfca3ebb3ac197617181d
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-05-06 12:42:37 +0530

    Formatting documents

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {
   getFirestore, collection, onSnapshot,
-  addDoc, deleteDoc, doc, 
+  addDoc, deleteDoc, doc,
   query, where,
   orderBy, serverTimestamp,
   getDoc,
@@ -13,14 +13,14 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDLa_nr_0c0kudQSzcGV5hkwq3WH2bRGgo",
-    authDomain: "freidea-pos.firebaseapp.com",
-    projectId: "freidea-pos",
-    storageBucket: "freidea-pos.appspot.com",
-    messagingSenderId: "317401437770",
-    appId: "1:317401437770:web:2657a877ea9fe88cf878b3",
-    measurementId: "G-M8RF0RL2FP"
-  }
+  apiKey: "AIzaSyDLa_nr_0c0kudQSzcGV5hkwq3WH2bRGgo",
+  authDomain: "freidea-pos.firebaseapp.com",
+  projectId: "freidea-pos",
+  storageBucket: "freidea-pos.appspot.com",
+  messagingSenderId: "317401437770",
+  appId: "1:317401437770:web:2657a877ea9fe88cf878b3",
+  measurementId: "G-M8RF0RL2FP"
+}
 
 initializeApp(firebaseConfig);
 
@@ -35,14 +35,14 @@ const colRef = collection(db, 'Saluni-fashion')
 
 // queries
 
-const q = query(colRef, orderBy('Price','desc'))  // , where("Name","==","Bhathiya")
+const q = query(colRef, orderBy('Price', 'desc'))  // , where("Name","==","Bhathiya")
 
 // real time collection data
 
 onSnapshot(q, (snapshot) => {
-  let salunifashion =[]
+  let salunifashion = []
   snapshot.docs.forEach((docs) => {
-salunifashion.push({...docs.data(), id: docs.id })
+    salunifashion.push({ ...docs.data(), id: docs.id })
   })
   console.log(salunifashion)
 })
@@ -50,47 +50,47 @@ salunifashion.push({...docs.data(), id: docs.id })
 //add items 
 
 const addProducts = document.querySelector('.add')
-addProducts.addEventListener('submit',(e) => {
-    e.preventDefault()
+addProducts.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-   addDoc(colRef, {
+  addDoc(colRef, {
     Name: addProducts.name.value,
     Size: addProducts.size.value,
-    Color:addProducts.color.value,
-    Price:addProducts.price.value,
+    Color: addProducts.color.value,
+    Price: addProducts.price.value,
     Created: serverTimestamp()
 
-   })
-   .then(() => {
-    addProducts.reset();
-   })
+  })
+    .then(() => {
+      addProducts.reset();
+    })
 
 })
 
 //delete items 
 
 const deleteProducts = document.querySelector('.delete')
-deleteProducts.addEventListener('submit',(e) =>{
-    e.preventDefault()
+deleteProducts.addEventListener('submit', (e) => {
+  e.preventDefault()
 
   const docRef = doc(db, 'Saluni-fashion', deleteProducts.id.value)
 
   deleteDoc(docRef)
-  .then(() => {
-  deleteProducts.reset()
-})
+    .then(() => {
+      deleteProducts.reset()
+    })
 })
 
 // get a single document
 
-const docRef = doc(db, 'saluni-fashion','3kHVyMlEtdtXFTJBs8kC')
+const docRef = doc(db, 'saluni-fashion', '3kHVyMlEtdtXFTJBs8kC')
 // getDoc(docRef)
 // .then((doc) => {
 //   console.log(doc.data(), doc.id)
 // })
 
-onSnapshot(docRef, ()=> {
-  
+onSnapshot(docRef, () => {
+
 })
 
 
@@ -98,11 +98,11 @@ const updateForm = document.querySelector('.update')
 updateForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
-const docRef = doc(db, 'Saluni-fashion', updateForm.id.value)
-   
-    updateDoc(docRef, {
-      Name: 'update name'
-    })
+  const docRef = doc(db, 'Saluni-fashion', updateForm.id.value)
+
+  updateDoc(docRef, {
+    Name: 'update name'
+  })
     .then(() => {
       updateForm.reset()
     })
@@ -116,15 +116,15 @@ singupForm = addEventListener('submit', (e) => {
 
   const email = singupForm.email.value       //grab the values for email andd password from frontend
   const password = singupForm.password.value
-  
-  createUserWithEmailAndPassword(auth, email, password )
-.then((cred) => {
-  console.log("User Created:", cred.user) 
-singupForm.reset()
 
-})
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log("User Created:", cred.user)
+      singupForm.reset()
 
-.catch((err) => {
-  console.log(err.message)
-})
+    })
+
+    .catch((err) => {
+      console.log(err.message)
+    })
 })
